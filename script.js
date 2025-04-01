7341f29a93dbe964e298ac9430d0abeca986eb69
kenji20th <amineelayachi22@outlook.com>
2025-04-01 23:13:51 +0200

    project-setup

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
+const ctxt = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+ctxt.fillStyle = 'white'
+ctxt.fillRect(0, 0, canvas.width, canvas.height)
