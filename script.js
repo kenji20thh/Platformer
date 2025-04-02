a8985373960d378a010252de44595802ae3ab345
kenji20th <amineelayachi22@outlook.com>
2025-04-02 03:10:37 +0200

    added-gravity

--- script.js
+++ script.js
@@ -4,5 +4,42 @@ const ctxt = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-ctxt.fillStyle = 'white'
-ctxt.fillRect(0, 0, canvas.width, canvas.height)
+const gravity = 0.5
+
+class Player {
+    constructor(position) {
+        this.position = position  
+        this.velocity = {
+            x:0,
+            y:1
+        }  
+    }
+    draw() {
+        ctxt.fillStyle = 'red'
+        ctxt.fillRect(this.position.x, this.position.y, 100, 100)
+    }
+    update() {
+        this.draw()
+        this.position.y += this.velocity.y
+        this.velocity.y += gravity
+    }
+}
+
+const player = new Player({
+    x:0,
+    y:0
+})
+
+const player2 =new Player({
+    x:100,
+    y:100
+})
+const animate = () => {
+    window.requestAnimationFrame(animate)
+    ctxt.fillStyle = 'white'
+    ctxt.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    player2.update()    
+}
+
+animate()
