319da334d6940ca1bd2d323037f9a73e179d0057
kenji20th <amineelayachi22@outlook.com>
2025-04-02 03:14:00 +0200

    velocity+stopping-at-botcanvas

--- script.js
+++ script.js
@@ -13,15 +13,20 @@ class Player {
             x:0,
             y:1
         }  
+        this.height = 100
     }
     draw() {
         ctxt.fillStyle = 'red'
-        ctxt.fillRect(this.position.x, this.position.y, 100, 100)
+        ctxt.fillRect(this.position.x, this.position.y, 100, this.height)
     }
     update() {
         this.draw()
         this.position.y += this.velocity.y
-        this.velocity.y += gravity
+        if (this.position.y + this.height + this.velocity.y< canvas.height) {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
     }
 }
 
